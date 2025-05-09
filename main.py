dab778c92e5e90b250ac2c9626e029476f293cea
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-05-09 23:56:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import asyncio
+from pyrogram import idle
 from forward import bot_1, userbot
 
 async def main():
-    await asyncio.gather(bot_1.start(), userbot.start())
+    await bot_1.start()
+    await userbot.start()
     print("Both bots started.")
-    await asyncio.Event().wait()  # Keeps the script alive
+
+    # Keep the program running
+    await idle()
+
+    # Optional: Stop clients gracefully on shutdown
+    await bot_1.stop()
+    await userbot.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
