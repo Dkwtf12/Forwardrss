d3a226efdb8b79cce380cadeadf9979abdcd0f3b
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-05-09 22:45:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,6 @@ USER_ID = int(os.getenv("USER_ID", "1568895149"))
 TARGET_GROUP_ID = int(os.getenv("TARGET_GROUP_ID", "-1002356176128"))  # Add your target group ID here
 
 TG_WORKERS = int(os.getenv("TG_WORKERS", "4"))
+
+# Session string for userbot
+SESSION_STRING = os.getenv("SESSION_STRING", "BQEw1ckAjbK5gm36s2Qs93i6XjLu9jZmbSGMV062W9Ddtd_A_vVhgjgJrpXzaqqDGiT5aBVi22SVbxMyVrFBaMyc6n7479OXUPwaywVL0PkRnqvgAhXRvnZ7fk6pdxktOCyRT-kGnb_GpU0wdZU3Sb_zBQKDx2Vj89o44u5N2j_J-EK0sLLscCKvalN4UXRkA-IYqjhI5i5b1EFiPPZFnDcJurNZknU4UHhvlc3GYj0gcgHQpC6xIWJbbi_RWHPYISz8eQZnZqGdpfYXYiZNzqSffP6l5ZB1Lko0nXegov7dNgCoHvG2Ud2vp2QDtFZGXjC-aaHIFsjqv9pPPVCWkcHkNhoyAwAAAAGnwwoiAA")
