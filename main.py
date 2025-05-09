e8b02b39f89ba9685693be5a510124c6499953aa
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-05-09 22:17:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import os
+from forward import bot_1, bot_2, start_forwarding, start_bot_2
+from pyrogram import idle
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    os.system("python bot.py")
+    start_forwarding(bot_1)
+    start_bot_2(bot_2)
+
+    bot_1.start()
+    bot_2.start()
+
+    logger.info("Both bots started and running.")
+    idle()
+
+    bot_1.stop()
+    bot_2.stop()
