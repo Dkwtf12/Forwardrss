4c7753130fa97e9a6fffb27208680d61328e3a89
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-05-09 13:46:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", "19977673"))
 API_HASH = os.getenv("API_HASH", "f75386c7aab88e2ad9b5de220fc0ceb4")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7601241825:AAGM8N5ea6bh-2sEQRV-0NqtTJ6fvbE1b0g")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5044558270:AAETnUGHgVriS9y_b5ImSC2qOeuRuBLkTp4")
 
 SOURCE_CHAT_ID = int(os.getenv("SOURCE_CHAT_ID", "-1002398179296"))  # Source channel/group ID
 DEST_CHAT_ID = int(os.getenv("DEST_CHAT_ID", "-1002477240145"))  # Destination channel/group ID
