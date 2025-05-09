66fe33787ca5dc131b21b1828fb0baa714235465
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-05-09 17:15:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = os.getenv("API_HASH", "f75386c7aab88e2ad9b5de220fc0ceb4")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "5044558270:AAETnUGHgVriS9y_b5ImSC2qOeuRuBLkTp4")
 
 SOURCE_CHAT_ID = int(os.getenv("SOURCE_CHAT_ID", "-1002440398569"))
-DEST_CHAT_ID = int(os.getenv("DEST_CHAT_ID", "-1002477240145"))
+DEST_CHAT_ID = int(os.getenv("DEST_CHAT_ID", "-1002332171379"))
 
 TAG_USERNAME = os.getenv("TAG_USERNAME", "zsbhere")
 USER_ID = int(os.getenv("USER_ID", "1568895149"))
